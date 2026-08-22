3ec4b5870bbb1ffc05209e8fcfbbd6bd3c8c4cc7
vivek-mulagaleti <vivek1492006@gmail.com>
2026-08-22 19:54:15 +0530

    delete product from html

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -14,7 +14,7 @@ cart.forEach((cartItem) => {
     }
   });
   cartSummeryHtml += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -37,7 +37,7 @@ cart.forEach((cartItem) => {
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary js-delete-link data-product-id=${matchingProduct.id}">
+                  <span class="delete-quantity-link link-primary js-delete-link "data-product-id=${matchingProduct.id}>
                     Delete
                   </span>
                 </div>
@@ -98,5 +98,11 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
   link.addEventListener("click", () => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
+
+
+    const container=document.querySelector(
+      `.js-cart-item-container-${productId}`);
+    console.log(container);
+    container.remove();
   });
 });
